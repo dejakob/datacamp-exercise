f0513ca2fbbca0dca825871b0ef54b92357c1a38
dejakob <jakob.kerkhove@gmail.com>
2024-08-16 19:06:50 +0200

    fix: Vertical alignment textarea android

--- components/TextArea/TextArea.tsx
+++ components/TextArea/TextArea.tsx
@@ -3,5 +3,13 @@ import { styles } from "./styles";
 import { ComponentProps } from "react";
 
 export const TextArea = (props: ComponentProps<typeof TextInput>) => {
-  return <TextInput {...props} multiline style={styles.textArea} testID="textArea" />;
+  return (
+    <TextInput
+      {...props}
+      textAlignVertical="top"
+      multiline
+      style={styles.textArea}
+      testID="textArea"
+    />
+  );
 };
