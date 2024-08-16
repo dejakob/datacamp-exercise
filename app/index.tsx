b02d29ce6fa30333aebced1269044f1d1786b68d
dejakob <jakob.kerkhove@gmail.com>
2024-08-16 12:35:23 +0200

    fix: Only update when pressing preview

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
 import { Button, MarkdownPreview, SplitScreen, TextArea } from "@/components";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { KeyboardAvoidingView } from "react-native";
 
 export default function LandingScreen() {
-    const [rawMarkdown, setRawMarkdown] = useState('');
+  const [rawMarkdown, setRawMarkdown] = useState("");
+  const [previewMarkdown, setPreviewMarkdown] = useState("");
+
+  const handleSubmit = useCallback(() => {
+    setPreviewMarkdown(rawMarkdown);
+  }, [rawMarkdown]);
 
   return (
     <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
-      <SplitScreen footer={<Button onPress={() => {}}>Render</Button>}>
+      <SplitScreen footer={<Button onPress={handleSubmit}>Render</Button>}>
         <TextArea value={rawMarkdown} onChangeText={setRawMarkdown} />
-        <MarkdownPreview content={rawMarkdown} />
+        <MarkdownPreview content={previewMarkdown} />
       </SplitScreen>
     </KeyboardAvoidingView>
   );
