0cfd36d1e39b30cf4b2b69afc48d845e3f6d3c0d
dejakob <jakob.kerkhove@gmail.com>
2024-08-16 12:44:26 +0200

    fix: Clean up and add support AsyncStorage

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,40 @@
 import { Button, MarkdownPreview, SplitScreen, TextArea } from "@/components";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { KeyboardAvoidingView } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function LandingScreen() {
   const [rawMarkdown, setRawMarkdown] = useState("");
   const [previewMarkdown, setPreviewMarkdown] = useState("");
 
-  const handleSubmit = useCallback(() => {
+  const loadFromStorage = useCallback(async () => {
+    try {
+      const mdContents = await AsyncStorage.getItem("md");
+
+      if (!mdContents) {
+        return;
+      }
+
+      setRawMarkdown(mdContents);
+      setPreviewMarkdown(mdContents);
+    } catch {
+      console.error("AsyncStorage not supported");
+    }
+  }, []);
+
+  const handleSubmit = useCallback(async () => {
     setPreviewMarkdown(rawMarkdown);
+
+    try {
+      await AsyncStorage.setItem("md", rawMarkdown);
+    } catch {
+      console.error("AsyncStorage not supported");
+    }
   }, [rawMarkdown]);
+
+  useEffect(() => {
+    loadFromStorage();
+  }, [loadFromStorage]);
 
   return (
     <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
